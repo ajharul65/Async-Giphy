8d674faaf50f6d07e3cc0d3db6da739a63e43824
ajharul64 <ajharul.islam@barclays.com>
2024-05-13 16:17:32 +0100

    final

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 require('dotenv').config();
-const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=SjQh08oaXOW9GKE1qRsR3Y5MVHUf6LhS&q=dog&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
 
 
 async function getImages(query){
-    try{
-    const response =  await fetch (endpoint)
+  
+    const response =  await fetch (endpoint + '&q='+query)
     const data = await response.json()
     
-    const img = data.results[0].url
-    console.log(img)
+    const img = data.data[0].url
+    console.log('img url: ',img)
 
     return img
 
 
 }
-catch (error){
-    console.error('There was an error fetching the image')
-}
-}
+
+getImages('cats')
 
